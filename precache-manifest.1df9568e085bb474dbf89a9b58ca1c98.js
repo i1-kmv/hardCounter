e00033daba3a5da9ea428ee7e4262ddf59445782
i1-kmv <i1-kmv@mail.ru>
2020-04-27 16:27:14 +0200

    Updates

---
+++ precache-manifest.1df9568e085bb474dbf89a9b58ca1c98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a92f07e2ce9c5c296b5d9548beffbb17",
+    "url": "/hardCounter/index.html"
+  },
+  {
+    "revision": "0d015471f5d16f39d5de",
+    "url": "/hardCounter/static/css/main.69312d8c.chunk.css"
+  },
+  {
+    "revision": "3de9519f219fb0140009",
+    "url": "/hardCounter/static/js/2.033fe4e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hardCounter/static/js/2.033fe4e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d015471f5d16f39d5de",
+    "url": "/hardCounter/static/js/main.123bb3dc.chunk.js"
+  },
+  {
+    "revision": "35261918fe124b0b41df",
+    "url": "/hardCounter/static/js/runtime-main.8e774ef3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hardCounter/static/media/logo.5d5d9eef.svg"
+  }
+]);
